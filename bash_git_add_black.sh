e3a56f5a7cc61549867be6700ebe6b34cca86818
Yen-KO Huang <jonney.huang@canterbury.ac.nz>
2021-03-12 00:22:05 +0000

    backup of git add wrapper

---
+++ bash_git_add_black.sh
@@ -0,0 +1,15 @@
+git_bin=`which git`
+function git () {
+    if [[ $1 == 'add' ]];then
+        args_list=${@:2}
+        echo $args_list
+        for i in $args_list;
+        do
+            if [[ $i == *.py ]];then
+                echo "black $i"
+                black $i
+            fi
+        done
+    fi  
+    bash -c "$git_bin $(printf ' %q' "$@")"
+}
